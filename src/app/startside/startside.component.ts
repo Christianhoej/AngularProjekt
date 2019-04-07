b977cfe5e26746ebb3975d1e7c589df7987abad5
Christianhoej <s175125@student.dtu.dk>
2019-04-07 20:51:33 +0200

    Forsøg med routing

---
+++ src/app/startside/startside.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-startside',
+  templateUrl: './startside.component.html',
+  styleUrls: ['./startside.component.css']
+})
+export class StartsideComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/trae/trae.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trae',
+  templateUrl: './trae.component.html',
+  styleUrls: ['./trae.component.css']
+})
+export class TraeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
