fe35bb5be76fa37410e763ac8241d6009e8406d1
Christianhoej <s175125@student.dtu.dk>
2019-04-08 14:36:53 +0200

    Oprettelse af komponenter

---
+++ src/app/detaljeret-annonce/detaljeret-annonce.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detaljeret-annonce',
+  templateUrl: './detaljeret-annonce.component.html',
+  styleUrls: ['./detaljeret-annonce.component.css']
+})
+export class DetaljeretAnnonceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/log-ind/log-ind.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-log-ind',
+  templateUrl: './log-ind.component.html',
+  styleUrls: ['./log-ind.component.css']
+})
+export class LogIndComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
