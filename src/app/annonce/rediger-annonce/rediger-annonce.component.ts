3e1936e98bc8994975a54e1bba1fec1fb3ad65f2
Gunn Hentze <s145494@student.dtu.dk>
2019-05-07 15:17:07 +0200

    rediger annonce

--- src/app/annonce/rediger-annonce/rediger-annonce.component.ts
+++ src/app/annonce/rediger-annonce/rediger-annonce.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Params} from '@angular/router';
+import {Annonce} from '../../models/annonce.model';
+import {AnnonceService} from '../annonce.service';
+import {KategoriService} from '../../startside/kategorier/kategori.service';
+import {Kategorier} from '../../startside/kategorier/kategorier.model';
 
 @Component({
   selector: 'app-rediger-annonce',
@@ -6,10 +11,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./rediger-annonce.component.css']
 })
 export class RedigerAnnonceComponent implements OnInit {
-
-  constructor() { }
+  id: string;
+  annonce: Annonce;
+  key: string;
+  kategorier: Kategorier[];
+  constructor(private annonceService: AnnonceService, private route: ActivatedRoute, private kategoriService: KategoriService) { }
 
   ngOnInit() {
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.key = 'id';
+          this.id = params[this.key];
+          this.annonceService.getAnnonce(this.id)
+            .subscribe(
+              (annonce: Annonce) => {this.annonce = annonce;
+              }
+            );
+        }
+      );
+    this.kategoriService.getKategorier()
+      .subscribe(
+        (kategorier: Kategorier[]) => {this.kategorier = kategorier;
+        }
+      )
+
+
   }
 
 }
