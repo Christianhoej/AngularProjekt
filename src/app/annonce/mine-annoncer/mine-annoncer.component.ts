90201b24dbb5903beef7c46cce2eed61022077dc
Gunn Hentze <s145494@student.dtu.dk>
2019-05-07 12:30:23 +0200

    med mine annoncer

---
+++ src/app/annonce/mine-annoncer/mine-annoncer.component.ts
@@ -0,0 +1,21 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {Annonce} from '../../models/annonce.model';
+
+@Component({
+  selector: 'app-mine-annoncer',
+  templateUrl: './mine-annoncer.component.html',
+  styleUrls: ['./mine-annoncer.component.css']
+})
+export class MineAnnoncerComponent implements OnInit {
+
+  @Input() minAnnonce: Annonce;
+  @Input() id: number;
+  @Input() index: number
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
